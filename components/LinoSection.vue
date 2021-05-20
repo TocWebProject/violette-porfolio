88281a2759f2831f2c36fc0e2d8529a750ce9401
Toc <tocwebtoc@gmail.com>
2021-05-20 18:02:00 +0200

    LinoSection: responsive layout & animation on scroll

--- components/LinoSection.vue
+++ components/LinoSection.vue
@@ -2,41 +2,167 @@
     <section id="linogravures" class="lino-section">
         <div class="section-container">
             <h1>Linogravures</h1>
-            <NuxtLink to="/labaigneuse">La baigneuse</NuxtLink>
-        </div>
+                <div class="baigneuse-top" id="baigneuse"></div>
+                <div class="baigneuse-container">
+                    <NuxtLink class="link-img-baigneuse" to="/labaigneuse">
+                        <div class="overlay-card-baigneuse"></div>
+                        <img  class="img-baigneuse" src="~assets/img/violette-baigneuse-vase.png" width="478" height="597" alt="linogravure noir et bleu" loading="lazy">
+                    </NuxtLink>
+                    <NuxtLink class="link-baigneuse" to="/labaigneuse">La baigneuse</NuxtLink>
+                </div>
+
+                <div class="nues-top" id="nues"></div>
+                <div class="nues-container">
+                    <NuxtLink class="link-nues" to="/nues">Nues</NuxtLink>
+                    <NuxtLink class="link-img-nues" to="/nues">
+                        <div class="overlay-card-nues"></div>
+                        <img  class="img-nues" src="~assets/img/violette-nues-plantes.png" width="478" height="597" alt="linogravure noir et bleu" loading="lazy">
+                    </NuxtLink>
+                </div>
+            </div>
     </section>
 </template>
 
 <script>
 export default {
     name:'LinoSection',
+    methods: {
+        animateOnScroll() {
+            const gsap = this.$gsap
+
+            gsap.to('.overlay-card-baigneuse', {
+                scrollTrigger: {
+                    trigger: '.overlay-card-baigneuse',
+                    start: 'top 80%',
+                    toggleClass: 'active',
+                    toggleActions: "restart resume resume reverse",
+                },
+                ease: 'power0.inOut',
+                height: 0,
+            })
+
+            gsap.to('.overlay-card-nues', {
+                scrollTrigger: {
+                    trigger: '.overlay-card-nues',
+                    start: 'top 80%',
+                    toggleClass: 'active',
+                    toggleActions: "restart resume resume reverse",
+                },
+                ease: 'power0.inOut',
+                height: 0,
+            })
+        }
+    },
+
+    mounted(){
+        this.animateOnScroll();
+    }
 }
 </script>
 
 <style lang="scss">
 .lino-section {
     background-color: #21409a;
-     color: #ffffff;
+    color: #ffffff;
+    padding: 60px 0px 60px 0px;
   
     .section-container {
-        height: 100vh;
         margin: 0 auto;
         max-width: 1400px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
+        height: auto;
 
         h1{
+            margin: 50px 0px;
             text-align: center;
             font-size: 90px;
             color: #ffffff;
-            margin-bottom: 30px;
+        }
+
+        // BAIGNEUSE CARD
+        .baigneuse-top {
+            height: 50px;
+        }
+
+        .baigneuse-container {
+            display: flex;
+            justify-items: center;
+            align-items: center;
+            height: auto;
+            margin-left: 99px;
+            
+
+            .link-baigneuse {
+                margin-left: 80px;
+                transform: translateY(-75px);
+            }
+
+            .link-img-baigneuse {
+                position: relative;
+                margin: 0px 20px;
+                overflow: visible;
+                
+
+                .img-baigneuse {
+                    max-width: 100%;
+                    height: auto;
+                }
+                .overlay-card-baigneuse {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background-color: #2849ac;
+                }
+            }
+        }
+
+        // NUES CARD
+        .nues-top {
+            height: 50px;
+            transform: translateY(-180px);
+        }
+
+        .nues-container {
+            display: flex;
+            justify-content: flex-end;
+            justify-items: center;
+            align-items: center;
+            height: auto;
+            margin-right: 99px;
+          
+
+            .link-nues {
+                margin-right: 80px;
+                transform: translateY(-75px);
+
+            }
+
+            .link-img-nues {
+                position: relative;
+                margin: 0px 20px;
+                transform: translateY(-180px);
+                
+                .img-nues {
+                    max-width: 100%;
+                    height: auto;
+                }
+                .overlay-card-nues {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background-color: #2849ac;
+                }
+            }
+
         }
 
         a {            
             text-decoration: none;
             color: white;
+            font-size: 25px;
 
             &:link {
             color: white;
@@ -58,15 +184,51 @@ export default {
 }
 
 
-// // Extra small devices (portrait phones, tablet less than 1024px)
-@media screen and (min-width: 100px) and (max-width: 1024px) { 
+// // Extra small devices (portrait phones, tablet, desktop less than 1220px)
+@media screen and (min-width: 100px) and (max-width: 1220px) { 
     .lino-section {
         .section-container {   
-            height: 100vh;
             min-height: 700px;
 
             h1 {
-                font-size: 55px;
+                font-size: 40px;
+                margin: 30px 0px;
+            }
+
+            // BAINGEUSE CARD
+            .baigneuse-container{
+                flex-direction: column;
+                margin-left: 0px;
+                margin-right: 0px;
+
+                .link-baigneuse {
+                    margin-left: 0px;
+                    transform: translateY(0px);
+                }
+            }
+            
+            // NUES CARD
+            .nues-top {
+                transform: translateY(0px);
+            }
+            .nues-container { 
+                flex-direction: column;
+                margin-left: 0px;
+                margin-right: 0px;
+
+                .link-nues {
+                    margin-right: 0px;
+                    transform: translateY(0px);
+                }
+
+                .link-img-nues {
+                    order: 1;
+                    transform: translateY(0px);
+                }
+
+                .link-nues {
+                    order: 2;
+                }
             }
         }
     }
