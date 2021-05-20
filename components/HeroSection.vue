e20c99ea61d6e7060a240b974ee68608adf48c00
Toc <tocwebtoc@gmail.com>
2021-05-20 18:45:54 +0200

    LinoSection: Improved toggleActions for overlay img animations

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -336,7 +336,7 @@ export default {
         .section-container  {
             .arrow-container {
                 left: 18px;
-                bottom: 55px;
+                bottom: 60px;
             }
 
             .hero-logo {
